fe2bf996e775e027f5b50beea81bdf2a32fd21ea
mathudak <mat.hudak@gmail.com>
2020-02-26 22:27:10 +0100

    Updates eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports =  {
     sourceType:  'module',  // Allows for the use of imports
   },
   "rules": {
-    "@typescript-eslint/no-empty-function": ["error", { "allow": ["constructors"] }],
+    "@typescript-eslint/no-empty-function": ["warn", { "allow": ["constructors"] }],
     "@typescript-eslint/indent": ["error", 2, {
         "SwitchCase": 1,
         "CallExpression": {"arguments": "first"},
